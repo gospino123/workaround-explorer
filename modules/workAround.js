c634262e71a8f415c214e7b324cf5d3f1c244296
gizmo <72733125+gospino123@users.noreply.github.com>
2021-09-10 17:00:20 -0400

    Use * imported fxns for workAround.js arrays

--- modules/workAround.js
+++ modules/workAround.js
@@ -6,28 +6,28 @@ import default salaryData from './salaryData.js';
 
 // Replace the empty array with the appropriate imported function/value
 const getAverageSalaryByRole = role => {
-    const roleData = [];
+    const roleData = getDataByRole();
     const salariesOfRole = roleData.map(obj => obj.salary);
     return calculateAverage(salariesOfRole);
   }
   
   // Replace the empty array with the appropriate imported function/value
   const getAverageSalaryByCompany = company => {
-    const companyData = [];
+    const companyData = getDataByCompany();
     const salariesAtCompany = companyData.map(obj => obj.salary);
     return calculateAverage(salariesAtCompany);
   }
   
   // Replace the empty array with the appropriate imported function/value
   const getSalaryAtCompany = (role, company) => {
-    const companyData = [];
+    const companyData = getDataByCompany();
     const roleAtCompany = companyData.find(obj => obj.role === role);
     return roleAtCompany.salary;
   }
   
   // Replace the empty array with the appropriate imported function/value
   const getIndustryAverageSalary = () => {
-    const allSalaries = [].map(obj => obj.salary);
+    const allSalaries = salaryData.map(obj => obj.salary);
     return calculateAverage(allSalaries);
   }
   
