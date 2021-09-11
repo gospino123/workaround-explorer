38926023e44883ef5197ef1f113080f04fc58fd8
gizmo <72733125+gospino123@users.noreply.github.com>
2021-09-10 20:43:52 -0400

    Import fxns from workAround to main

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // TODO: Add your import statements here.
 // Import getRoles and getCompanies from salaryData.js
 import {getRoles, getCompanies} from './modules/salaryData.js';
+import {getAverageSalaryByRole, getAverageSalaryByCompany, getSalaryAtCompany, getIndustryAverageSalary} from './modules/workAround.js';
 
 // TODO: Get the companies and roles using the salaryData module.
 const companies = getCompanies();
